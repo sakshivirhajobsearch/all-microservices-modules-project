75b4841828c9316fb593e4e9672f1b850c320c0c
Anurag Bharti <anuragvasubharti@gmail.com>
2025-03-27 01:48:03 +0530

    Added new modules

---
+++ monitoring/src/main/java/com/example/monitoring/MonitoringApplication.java
@@ -0,0 +1,23 @@
+package com.example.monitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class MonitoringApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(MonitoringApplication.class, args);
+	}
+}
+
+@RestController
+@RequestMapping("/")
+class HomeController {
+	@GetMapping
+	public String home() {
+		return "Welcome to the Monitoring Service!";
+	}
+}
